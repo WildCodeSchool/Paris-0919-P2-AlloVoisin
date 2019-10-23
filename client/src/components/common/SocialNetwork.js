9409810e182e8975655dfb3b0298d1a511efb541
Yam Clement <yamtzunhei@gmail.com>
2019-10-23 11:25:42 +0200

    finish social

---
+++ client/src/components/common/SocialNetwork.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+
+export default function SocialNetwork() {
+  return (
+    <div>
+    <p>Icon</p>
+      <FontAwesomeIcon icon={faFacebook}/>
+    </div>
+  )
+}
