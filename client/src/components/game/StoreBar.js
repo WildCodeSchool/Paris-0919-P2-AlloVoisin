39722859aca91e077c42e4d3cdd2d0813a3b8b4f
Yam Clement <yamtzunhei@gmail.com>
2019-10-23 12:04:41 +0200

    finish store-bar

--- client/src/components/game/StoreBar.js
+++ client/src/components/game/StoreBar.js
@@ -7,12 +7,11 @@ const StoreBar = () => {
 
         <div className="store-bar-container">
           <ul className="img-container">
-              <li className="img-weapons"></li>
-              <li className="img-skins"><img src="#" alt="skin"/></li>
-              <li className="img-characters"><img src="#" alt="character"/></li>
-              <li className="img-skills"><img src="#" alt="skill"/></li>
+              <li className="img-weapons"><img src="https://image.noelshack.com/fichiers/2019/43/3/1571823611-weapons.png" alt="weapon"/></li>
+              <li className="img-skins"><img src="https://image.noelshack.com/fichiers/2019/43/3/1571823600-skins.png" alt="skin"/></li>
+              <li className="img-characters"><img src="https://image.noelshack.com/fichiers/2019/43/3/1571823595-characters.png" alt="character"/></li>
+              <li className="img-skills"><img src="https://image.noelshack.com/fichiers/2019/43/3/1571823603-skills.png" alt="skill"/></li>
           </ul>
-          <img src="./marvel.png" alt="weapon"/>
        </div>
     )
 }
