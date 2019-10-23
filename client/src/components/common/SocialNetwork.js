05bc935895609641d1085092fd8dac3a8f1b5647
adamsbah <ballyadamsco@gmail.com>
2019-10-23 11:50:22 +0200

    modifier social

--- client/src/components/common/SocialNetwork.js
+++ client/src/components/common/SocialNetwork.js
@@ -10,10 +10,10 @@ import {
 const SocialNetwork = () =>{
     return (
         <div className="social-container">
-           <a href="facebook.com"><FontAwesomeIcon icon={faFacebook} size="2x" /></a> 
-            <a href="instagram.com"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
-            <a href="youytube.com"><FontAwesomeIcon icon={faYoutube} size="2x" /></a>
-            <a href="linkeding.com"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+           <a href="https://www.facebook.com"><FontAwesomeIcon icon={faFacebook} size="2x" /></a> 
+            <a href="https://www.instagram.com"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+            <a href="https://www.youtube.com"><FontAwesomeIcon icon={faYoutube} size="2x" /></a>
+            <a href="https://www.linkedin.com"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
         </div>
     )
 }
