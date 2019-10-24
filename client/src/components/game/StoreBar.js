5b7a91576ab0344a7c6cc9aa13564a718e7d2cbd
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-10-24 17:52:25 +0200

    made changes to how store show

--- client/src/components/game/StoreBar.js
+++ client/src/components/game/StoreBar.js
@@ -1,41 +1,42 @@
 import React from "react";
-import {NavLink} from 'react-router-dom';
+// import {NavLink} from 'react-router-dom';
 import "./StoreBar.css";
 
-const StoreBar = () => {
+class StoreBar extends React.Component {
 
-  const handleClick = () => {
+  // const handleClick = () => {
     
-  };
-
+  // };
+  render() {
   return (
     <div className="store-bar-container">
       <ul className="img-container">
       
-        <NavLink className="nav-link" id="img-skins" to="/store/skins">
+        <div className="nav-link" id="img-skins"  onClick={this.props.showStoreSkins}>
           <img
             src="https://image.noelshack.com/fichiers/2019/43/4/1571922941-skins.png"
             alt="skin"
           />
           <p>Skins</p>
-        </NavLink>
-        <NavLink className="nav-link" id="img-characters" to="/store/characters">
+        </div>
+        <div className="nav-link" id="img-characters" onClick={this.props.showStoreCharacters}>
           <img
             src="https://image.noelshack.com/fichiers/2019/43/4/1571922941-characters.png"
             alt="character"
           />
           <p>Characters</p>
-        </NavLink>
-        <NavLink className="nav-link" id="img-skills" to="/store">
+        </div>
+        <div className="nav-link" id="img-skills" onClick={this.props.showStoreSkills}>
           <img
             src="https://image.noelshack.com/fichiers/2019/43/4/1571922941-skills.png"
             alt="skill"
           />
           <p>Skills</p>
-        </NavLink>
+        </div>
       </ul>
     </div>
   );
+  }
 };
 
 export default StoreBar;
