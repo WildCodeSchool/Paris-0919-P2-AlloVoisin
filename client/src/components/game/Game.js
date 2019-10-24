13afd6849f028af764ba29ea18fa18d76ce2b22a
Audrey Guiongo <audreymorganedto@gmail.com>
2019-10-24 11:26:03 +0200

    update store-bar

--- client/src/components/game/Game.js
+++ client/src/components/game/Game.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Store from './store/Store';
 import StoreBar from './StoreBar';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Characters from './store/Characters';
---
+++ client/src/components/game/Villain.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Villain() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
