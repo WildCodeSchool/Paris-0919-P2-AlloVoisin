bc2c8da21a5358b36e4e6ce1dd704099f58664fc
Felix <flxheng@gmail.com>
2019-10-24 15:56:00 +0200

    first draft projet02 navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavBar from './components/common/NavBar';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <NavBar />
     </div>
   );
 }
