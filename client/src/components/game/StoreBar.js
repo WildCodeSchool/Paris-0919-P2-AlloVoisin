44e8daabee8fc8369002685591b1a48f7c4f8fd0
Audrey Guiongo <audreymorganedto@gmail.com>
2019-10-24 15:25:27 +0200

    change store-bar

--- client/src/components/game/StoreBar.js
+++ client/src/components/game/StoreBar.js
@@ -11,30 +11,24 @@ const StoreBar = () => {
   return (
     <div className="store-bar-container">
       <ul className="img-container">
-        <NavLink className="nav-link" id="img-weapons" to="/store">
-          <img
-            src="https://image.noelshack.com/fichiers/2019/43/3/1571823611-weapons.png"
-            alt="weapon"
-          />
-          <p>Weapons</p>
-        </NavLink>
+      
         <NavLink className="nav-link" id="img-skins" to="/store/skins">
           <img
-            src="https://image.noelshack.com/fichiers/2019/43/3/1571823600-skins.png"
+            src="https://image.noelshack.com/fichiers/2019/43/4/1571922941-skins.png"
             alt="skin"
           />
           <p>Skins</p>
         </NavLink>
         <NavLink className="nav-link" id="img-characters" to="/store/characters">
           <img
-            src="https://image.noelshack.com/fichiers/2019/43/3/1571823595-characters.png"
+            src="https://image.noelshack.com/fichiers/2019/43/4/1571922941-characters.png"
             alt="character"
           />
           <p>Characters</p>
         </NavLink>
         <NavLink className="nav-link" id="img-skills" to="/store">
           <img
-            src="https://image.noelshack.com/fichiers/2019/43/3/1571823603-skills.png"
+            src="https://image.noelshack.com/fichiers/2019/43/4/1571922941-skills.png"
             alt="skill"
           />
           <p>Skills</p>
