9017d2983bc62dcf8d5c58650b55f5cbd40f7b95
Yam Clement <yamtzunhei@gmail.com>
2019-10-25 10:37:18 +0200

    add padding and refactoring

--- client/src/components/common/SocialNetwork.js
+++ client/src/components/common/SocialNetwork.js
@@ -6,22 +6,25 @@ import {
   faYoutube,
   faLinkedin
 } from "@fortawesome/free-brands-svg-icons";
+import "./SocialNetwork.css";
 
 const SocialNetwork = () => {
   return (
     <div className="social-container">
       <a href="http://facebook.com" target="_blank">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
+        <FontAwesomeIcon icon={faFacebook} size="2x" className="social-icon" />
       </a>
       <a href="http://instagram.com" target="_blank">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
+        <FontAwesomeIcon icon={faInstagram} size="2x" className="social-icon" />
       </a>
       <a href="http://youtube.com" target="_blank">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
+        <FontAwesomeIcon icon={faYoutube} size="2x" className="social-icon" />
       </a>
       <a href="http://linkedin.com" target="_blank">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        <FontAwesomeIcon icon={faLinkedin} size="2x" className="social-icon" />
       </a>
     </div>
   );
 };
+
+export default SocialNetwork;
