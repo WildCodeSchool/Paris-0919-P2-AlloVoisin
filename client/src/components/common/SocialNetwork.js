50e7e5af3066ee27dd636653bcd703931a0c3ae4
Yam Clement <yamtzunhei@gmail.com>
2019-10-25 09:51:07 +0200

    refactor social links

--- client/src/components/common/SocialNetwork.js
+++ client/src/components/common/SocialNetwork.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
-export default function SocialNetwork() {
+const SocialNetwork = () => {
   return (
-    <div>
-    <p>Icon</p>
-      <FontAwesomeIcon icon={faFacebook}/>
+    <div className="social-container">
+      <a href="http://facebook.com" target="_blank">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="http://instagram.com" target="_blank">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      <a href="http://youtube.com" target="_blank">
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="http://linkedin.com" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
     </div>
-  )
-}
+  );
+};
+
+export default SocialNetwork;
