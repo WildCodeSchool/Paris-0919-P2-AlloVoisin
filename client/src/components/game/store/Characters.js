148fef001e1116864af25fecde03bbf5a01aa597
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-10-28 14:43:51 +0100

    the cross image now close the store on click

--- client/src/components/game/store/Characters.js
+++ client/src/components/game/store/Characters.js
@@ -1,65 +1,72 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Characters.css";
 
-const Characters = () => {
-  return (
-    <div className="items-container">
-      <h2>Store</h2>
-      <img src="https://image.noelshack.com/fichiers/2019/43/4/1571929738-cross.png" alt="cross" onClick={this.props.showStoreSkins}/>
-      <ul className="items-list-container">
-        <li className="item-container">
-          <div className="background-character">
-            <img
-              src="https://image.noelshack.com/fichiers/2019/43/3/1571836551-thor.png"
-              alt="thor"
-            />
-          </div>
-          <p>Thor</p>
-          <button className="Store-button">Hire</button>
-        </li>
-        <li className="item-container">
-          <div className="background-character">
-            <img
-              src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-spider-man.png"
-              alt="spider-man"
-            />
-          </div>
-          <p>Spider Man</p>
-          <button className="Store-button">Hire</button>
-        </li>
-        <li className="item-container">
-          <div className="background-character">
-            <img
-              src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-ms-marvel.png"
-              alt="ms-marvel"
-            />
-          </div>
-          <p>Ms. Marvel</p>
-          <button className="Store-button">Hire</button>
-        </li>
-        <li className="item-container">
-          <div className="background-character">
-            <img
-              src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-hulk.png"
-              alt="hulk"
-            />
-          </div>
-          <p>Hulk</p>
-          <button className="Store-button">Hire</button>
-        </li>
-        <li className="item-container">
-          <div className="background-character">
-            <img
-              src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-black-willow.png"
-              alt="black-widow"
-            />
-          </div>
-          <p>Black Widow</p>
-          <button className="Store-button">Hire</button>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class Characters extends Component {
+  render() {
+    return (
+      <div className="items-container">
+        <h2>Store</h2>
+        <img
+          src="https://image.noelshack.com/fichiers/2019/43/4/1571929738-cross.png"
+          alt="cross"
+          class="cross"
+          onClick={this.props.showStoreCharacters}
+        />
+        <ul className="items-list-container">
+          <li className="item-container">
+            <div className="background-character">
+              <img
+                src="https://image.noelshack.com/fichiers/2019/43/3/1571836551-thor.png"
+                alt="thor"
+              />
+            </div>
+            <p>Thor</p>
+            <button className="Store-button">Hire</button>
+          </li>
+          <li className="item-container">
+            <div className="background-character">
+              <img
+                src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-spider-man.png"
+                alt="spider-man"
+              />
+            </div>
+            <p>Spider Man</p>
+            <button className="Store-button">Hire</button>
+          </li>
+          <li className="item-container">
+            <div className="background-character">
+              <img
+                src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-ms-marvel.png"
+                alt="ms-marvel"
+              />
+            </div>
+            <p>Ms. Marvel</p>
+            <button className="Store-button">Hire</button>
+          </li>
+          <li className="item-container">
+            <div className="background-character">
+              <img
+                src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-hulk.png"
+                alt="hulk"
+              />
+            </div>
+            <p>Hulk</p>
+            <button className="Store-button">Hire</button>
+          </li>
+          <li className="item-container">
+            <div className="background-character">
+              <img
+                src="https://image.noelshack.com/fichiers/2019/43/3/1571836568-black-willow.png"
+                alt="black-widow"
+              />
+            </div>
+            <p>Black Widow</p>
+            <button className="Store-button">Hire</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Characters;
--- client/src/components/game/store/Skills.js
+++ client/src/components/game/store/Skills.js
@@ -1,13 +1,20 @@
-import React from "react";
-import './Skills.css'
+import React, {Component} from "react";
+import "./Skills.css";
 
-const Skills = () => {
-  return (
-    <div className="items-container">
-    <h2>Store</h2>
-    <img src="https://image.noelshack.com/fichiers/2019/43/4/1571929738-cross.png" alt="cross" onClick={this.props.showStoreSkills}/>
-    </div>
-  );
-};
+class Skills extends Component {
+  render() {
+    return (
+      <div className="items-container">
+        <h2>Store</h2>
+        <img
+          src="https://image.noelshack.com/fichiers/2019/43/4/1571929738-cross.png"
+          alt="cross"
+          class="cross"
+          onClick={this.props.showStoreSkills}
+        />
+      </div>
+    );
+  }
+}
 
 export default Skills;
