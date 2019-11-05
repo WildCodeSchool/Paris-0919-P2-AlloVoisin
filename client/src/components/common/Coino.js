6b8e076cc29c9ee46bdf1961b3e5d42e8eba60ca
adamsbah <ballyadamsco@gmail.com>
2019-11-05 11:02:11 +0100

    add coins on villain death

---
+++ client/src/components/common/Coino.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import React, { Component } from 'react'
+
+export default class Coino extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
