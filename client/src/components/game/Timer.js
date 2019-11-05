972b3f61cbedf39da70391b43fb67dad798f91b8
Felix <flxheng@gmail.com>
2019-11-05 10:39:58 +0100

    init clickCounter

---
+++ client/src/components/game/Timer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Timer.css'
+
+class Timer extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default Timer 
