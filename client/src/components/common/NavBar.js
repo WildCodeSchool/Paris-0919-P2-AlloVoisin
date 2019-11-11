a81e4e50562824fe1d927db2dbb7847659245ad6
Yam Clement <yamtzunhei@gmail.com>
2019-11-11 19:12:16 +0100

    add link to game in navbar

--- client/src/components/common/NavBar.js
+++ client/src/components/common/NavBar.js
@@ -1,47 +1,51 @@
-import React, { Component } from 'react';
-import './NavBar.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 //import { is } from '@babel/types';
 //import { isTemplateElement } from '@babel/types';
 
 class NavBar extends Component {
-    state = {
-            isSidebarOpen: false
-        }
-    
-    handleMenuButtonClick = () => {
-        this.setState({ 
-            isSidebarOpen: !this.state.isSidebarOpen 
-        });
-    }
+  state = {
+    isSidebarOpen: false
+  };
 
-    render() {
-        return(
-            <div className="container">
-                <div className="menu-button" onClick={this.handleMenuButtonClick}>
-                    <img className="icon" src="https://image.noelshack.com/fichiers/2019/44/2/1572343624-logo.png" alt="ironman"/>
-                </div>
+  handleMenuButtonClick = () => {
+    this.setState({
+      isSidebarOpen: !this.state.isSidebarOpen
+    });
+  };
 
-                   <nav  className={this.state.isSidebarOpen ? 'nav show' : 'nav'}>
-                            <ul className="menu-items">
-                                <Link to='/'>
-                                    <li className="menu-list">
-                                        <span className="menu-link" >Game</span>
-                                    </li>
-                                    </Link>
-                                    <li className="menu-list">
-                                        <span className="menu-link" >Rules</span>
-                                    </li>
-                                    <Link to='/about'>
-                                    <li className="menu-list">
-                                        <span className="menu-link" >About</span>
-                                    </li>
-                                    </Link>
-                            </ul>
-                     </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="menu-button" onClick={this.handleMenuButtonClick}>
+          <img
+            className="icon"
+            src="https://image.noelshack.com/fichiers/2019/44/2/1572343624-logo.png"
+            alt="ironman"
+          />
+        </div>
+
+        <nav className={this.state.isSidebarOpen ? "nav show" : "nav"}>
+          <ul className="menu-items">
+            <Link to="/game">
+              <li className="menu-list">
+                <span className="menu-link">Game</span>
+              </li>
+            </Link>
+            <li className="menu-list">
+              <span className="menu-link">Rules</span>
+            </li>
+            <Link to="/about">
+              <li className="menu-list">
+                <span className="menu-link">About</span>
+              </li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
