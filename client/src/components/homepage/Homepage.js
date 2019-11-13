1ceba037216b49728d8062872121f97049025c74
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-11-13 15:30:22 +0100

    fixes

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -29,7 +29,7 @@ export default class Homepage extends Component {
         ) : null}
         <ReactPlayer
           url="https://www.youtube.com/embed/4vfGifZY85M"
-          playing={this.state.playing}
+          playing={true}
           width="0"
           height="0"
         />
